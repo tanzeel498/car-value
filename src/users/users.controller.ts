3194a5640a400fa9e2add0c2affbe4d95399c148
Tanzeel Ur Rehman <tanzeel498@gmail.com>
2024-08-28 03:59:48 +0500

    create user & report entity and setup validationPipe for PostUserBody

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { CreateUserDto } from './dtos/create-user.dto';
 
-@Controller('users')
-export class UsersController {}
+@Controller('auth')
+export class UsersController {
+  constructor(public usersService: UsersService) {}
+
+  @Post()
+  createUser(@Body() body: CreateUserDto) {
+    console.log(body);
+  }
+}
