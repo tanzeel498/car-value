bee16e25cfd3b2fb6183f5aa6561e2870c1587f9
Tanzeel Ur Rehman <tanzeel498@gmail.com>
2024-09-23 21:18:32 +0500

    add globalPipe & custom middleware to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,9 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { APP_PIPE } from '@nestjs/core';
+import { Module, ValidationPipe, MiddlewareConsumer } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppController } from './app.controller';
+const cookieSession = require('cookie-session');
+import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ReportsModule } from './reports/reports.module';
 import { User } from './users/user.entity';
@@ -19,6 +21,19 @@ import { Report } from './reports/report.entity';
     ReportsModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    { provide: APP_PIPE, useValue: new ValidationPipe({ whitelist: true }) },
+  ],
 })
-export class AppModule {}
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(
+        cookieSession({
+          keys: ['some1new2randoM3set4OF5keys'],
+        }),
+      )
+      .forRoutes('*');
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,8 @@
 import { NestFactory } from '@nestjs/core';
-import { ValidationPipe } from '@nestjs/common';
-const cookieSession = require('cookie-session');
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use(
-    cookieSession({
-      keys: ['some1random2key3'],
-    }),
-  );
-  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   await app.listen(3005);
 }
 bootstrap();
