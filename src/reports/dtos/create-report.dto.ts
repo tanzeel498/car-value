3ab55596201a5cb7063f3ca547a7e976ed81ea45
Tanzeel Ur Rehman <tanzeel498@gmail.com>
2024-09-24 19:20:52 +0500

    add post /reports, relationship b/w user and reports, and serialize the returned report

---
+++ src/reports/dtos/create-report.dto.ts
@@ -0,0 +1,37 @@
+import {
+  IsString,
+  IsNumber,
+  Min,
+  Max,
+  IsLatitude,
+  IsLongitude,
+} from 'class-validator';
+
+export class CreateReportDto {
+  @IsString()
+  make: string;
+
+  @IsString()
+  model: string;
+
+  @IsNumber()
+  @Min(1930)
+  @Max(new Date().getFullYear())
+  year: number;
+
+  @IsLatitude()
+  lat: number;
+
+  @IsLongitude()
+  lng: number;
+
+  @IsNumber()
+  @Min(0)
+  @Max(1000000)
+  mileage: number;
+
+  @IsNumber()
+  @Min(0)
+  @Max(1000000)
+  price: number;
+}
---
+++ src/reports/dtos/report.dto.ts
@@ -0,0 +1,31 @@
+import { Expose, Transform } from 'class-transformer';
+
+export class ReportDto {
+  @Expose()
+  id: number;
+
+  @Expose()
+  price: number;
+
+  @Expose()
+  year: number;
+
+  @Expose()
+  lat: number;
+
+  @Expose()
+  lng: number;
+
+  @Expose()
+  make: string;
+
+  @Expose()
+  model: string;
+
+  @Expose()
+  mileage: number;
+
+  @Expose()
+  @Transform(({ obj }) => obj.user.id) // obj is the report object
+  userId: number;
+}
